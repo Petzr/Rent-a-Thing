9cc50f8067266eed7a03765296239cc9b48b2207
Petzr <peterjansen812@gmail.com>
2022-05-10 11:08:35 +0200

    gemaakt van basic product

--- src/main/java/source_code/Product.java
+++ src/main/java/source_code/Product.java
@@ -1,4 +1,26 @@
 package source_code;
 
 public abstract class Product {
+
+    private String naam;    private boolean opVoorraad;
+    private double verhuurPrijs;
+    private double verzekeringPrijs;
+
+    public Product(double verhuurPrijs, double verzekeringPrijs) {
+        this.opVoorraad = true;
+        this.verhuurPrijs = verhuurPrijs;
+        this.verzekeringPrijs = verzekeringPrijs;
+    }
+
+    public boolean isOpVoorraad() {
+        return opVoorraad;
+    }
+
+    public double getVerhuurPrijs(int aantalDagen) {
+        return verhuurPrijs * aantalDagen;
+    }
+
+    public double getVerzekeringPrijs(int aantalDagen) {
+        return verzekeringPrijs * aantalDagen;
+    }
 }
