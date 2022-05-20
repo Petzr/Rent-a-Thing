682776ca716e7afe0798fe0b1931c267ac0b3f52
Petzr <peterjansen812@gmail.com>
2022-05-20 19:50:42 +0200

    Seeder used in main methode

--- src/main/java/source_code/Main.java
+++ src/main/java/source_code/Main.java
@@ -1,17 +1,18 @@
 package source_code;
 
 import source_code.products.Boormachine;
+import source_code.products.Product;
 import source_code.products.Vrachtwagen;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Boormachine boormachine = new Boormachine("merk", "type");
-        Vrachtwagen vrachtwagen = new Vrachtwagen(500, 1000);
+        Seeder seeder = new Seeder();
+        ArrayList<Product> products = seeder.getproducts();
 
-        System.out.println(boormachine.getVerhuurPrijs(3, false));
-        System.out.println(vrachtwagen.getVerhuurPrijs(3, false));
-
-        System.out.println(boormachine);
-
+        for (Product product : products) {
+            System.out.println(product);
+        }
     }
 }
