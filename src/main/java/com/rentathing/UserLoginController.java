c283f8576e092de5d896af27baa920101503059e
Petzr <peterjansen812@gmail.com>
2022-05-21 21:23:59 +0200

    een prachtig inlog scherm gemaakt voor users en daarbij skeleton van controller gemaakt

--- src/main/java/com/rentathing/UserLoginController.java
+++ src/main/java/com/rentathing/UserLoginController.java
@@ -1,4 +1,21 @@
 package com.rentathing;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class UserLoginController {
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    void loginButtonPressed(ActionEvent event) {
+
+    }
+
 }
