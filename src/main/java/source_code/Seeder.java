1ed73503fc9911d3449f8a7b2664c151180fb0ee
Petzr <peterjansen812@gmail.com>
2022-05-22 20:40:23 +0200

    factory toegepast in de Seeder.java

--- src/main/java/source_code/Seeder.java
+++ src/main/java/source_code/Seeder.java
@@ -5,6 +5,7 @@ import source_code.products.Boormachine;
 import source_code.products.PersonenAuto;
 import source_code.products.Product;
 import source_code.products.Vrachtwagen;
+import source_code.products.factory.ProductFactory;
 
 import java.util.ArrayList;
 
@@ -14,13 +15,13 @@ public class Seeder {
     private final ArrayList<Medewerker> medewerkers = new ArrayList<>();
 
     public Seeder () {
-        products.add (new Boormachine("Bosch", "GSB 18V-55 Professional"));
+        products.add (ProductFactory.createBoormachine("Bosch", "GSB 18V-55 Professional"));
         products.add (new Boormachine("Makita", "HP457DWE"));
         products.add (new Boormachine("Hitachi", "DS18DJLWC "));
-        products.add (new PersonenAuto("Audi", 1200));
+        products.add (ProductFactory.createPersonenAuto("Audi", 1200));
         products.add (new PersonenAuto("Volkswagen", 1300));
         products.add (new PersonenAuto("Ford", 1050));
-        products.add (new Vrachtwagen(22000, 3000));
+        products.add (ProductFactory.createVrachtwagen(22000, 3000));
         products.add (new Vrachtwagen(18000, 2700));
         products.add (new Vrachtwagen(15000, 2500));
 
