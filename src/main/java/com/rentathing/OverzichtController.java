61f2f9b895a62646883f7952ee3d08a58900c563
Petzr <peterjansen812@gmail.com>
2022-06-03 16:25:56 +0200

    werkende overzicht lijst

--- src/main/java/com/rentathing/OverzichtController.java
+++ src/main/java/com/rentathing/OverzichtController.java
@@ -1,18 +1,24 @@
 package com.rentathing;
 
 import com.rentathing.IControllerInfo;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import source_code.Bedrijf;
 import source_code.people.Medewerker;
 import source_code.products.Product;
+import source_code.products.Vrachtwagen;
 import source_code.products.factory.ProductFactory;
 
 import java.net.URL;
@@ -24,15 +30,11 @@ public class OverzichtController implements IControllerInfo, Initializable {
     private Medewerker medewerker;
 
     @FXML
-    private TableColumn<?, ?> beschrijvingColumn;
-    @FXML
     private Label errorMessage;
     @FXML
     private Label medewerkerLabel;
     @FXML
-    private TableColumn<?, ?> productenColumn;
-    @FXML
-    private TableView<?> productenTable;
+    private ListView<Product> productenList;
 
     @FXML
     void gaNaarDetailProduct(ActionEvent event) {
@@ -66,6 +68,8 @@ public class OverzichtController implements IControllerInfo, Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        for (Product product : bedrijf.getProducts()) productenList.getItems().add(product);
+
         errorMessage.setVisible(false);
     }
 }
