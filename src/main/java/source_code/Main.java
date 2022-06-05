96a9cedac7d15a8eb2b7b575795a73de09df3696
Petzr <peterjansen812@gmail.com>
2022-06-05 22:45:40 +0200

    debug en comments

--- src/main/java/source_code/Main.java
+++ src/main/java/source_code/Main.java
@@ -6,11 +6,17 @@ import source_code.products.Product;
 
 public class Main {
     public static void main(String[] args) {
+        // hier word een bedrijf gemaakt met producten en medewerkers door de seeder
         Bedrijf bedrijf = new Bedrijf(new Seeder());
 
+        // een medewerker wordt bepaald door het inloggen bij het bedrijf
         Medewerker medewerker = bedrijf.login("123", "123");
 
-        medewerker.addVerhuurdeProduct(new Seeder().getProduct(5));
+        // die medewerker huurt een product uit het bedrijf
+        medewerker.addVerhuurdeProduct(bedrijf.getProducts().get(1));
+        medewerker.addVerhuurdeProduct(bedrijf.getProducts().get(3));
+
+        // en hier wordt per product van de medewerker de verhuurprijs berekent en laten zien
         for (Product product : medewerker.getVerhuurdeProducten()) {
             System.out.println(product);
             System.out.println(product.getVerhuurPrijs(4, true));
